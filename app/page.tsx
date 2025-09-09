d7876f367b2c4a1bc413c9a1e10cf8578aa94375
Enclair743 <eclair.hsr@gmail.com>
2025-09-09 14:12:42 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useAuthCalendar } from "../src/context/AuthCalendarContext";
 
 export default function Home() {
+  const { user } = useAuthCalendar() || {};
   const router = useRouter();
 
   useEffect(() => {
-    router.replace("/dashboard");
-  }, [router]);
+    if (!user) {
+      router.replace("/login");
+    } else {
+      router.replace("/dashboard");
+    }
+  }, [user, router]);
 
   return null;
 }
